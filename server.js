caac5240025d33ac6faebb041c284aea3f43b96f
Robodlan <azuldevs@zohomail.com>
2026-01-04 19:57:18 -0400

    sever

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Hello Node!')
+import http from 'node:http';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.end("server is running");
+})
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
